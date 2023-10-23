856f509fd15c0197799f888d2a75e7b3803b4e82
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2023-10-23 23:58:26 +0900

    02_05完了

--- SourceFiles/TextureConverter.cpp
+++ SourceFiles/TextureConverter.cpp
@@ -2,11 +2,19 @@
 #include <Windows.h>
 #include <vector>
 
+using namespace DirectX;
+
 void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
 {
-	// ファイルパスをワイド文字列に変換
+	// ファイルパスをワイド文字列に変換する
 	std::wstring wfilePath = ConvertMultiByteStringToWideString(filePath);
-	// テクスチャを読み込む
+
+	// WICテクスチャのロード
+	HRESULT result = LoadFromWICFile(wfilePath.c_str(), WIC_FLAGS_NONE, &metadata, scratchImage);
+	assert(SUCCEEDED(result));
+
+	// フォルダパスとファイル名を分離する
+	SeparateFilePath(wfilePath);
 }
 
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString)
@@ -24,9 +32,71 @@ std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::str
 	return wString;
 }
 
+void TextureConverter::SeparateFilePath(const std::wstring& filePath_)
+{
+	size_t pos1;
+	std::wstring exceptExt;
+
+	// 区切り文字'.'が出てくる一番最後の部分を検索
+	pos1 = filePath_.rfind('.');
+	// 検索がヒットしたら
+	if (pos1 != std::wstring::npos)
+	{
+		// 区切り文字の後ろをファイル拡張子として保存 
+		fileExt = filePath_.substr(pos1 + 1, filePath_.size() - pos1 - 1);
+		// 区切り文字の前までを抜き出す
+		exceptExt = filePath_.substr(0, pos1);
+	}
+	else
+	{
+		fileExt = L"";
+		exceptExt = filePath_;
+	}
+
+	// 区切り文字'\\'が出てくる一番最後の部分を検索
+	pos1 = exceptExt.rfind('\\');
+	if (pos1 != std::wstring::npos)
+	{
+		// 区切り文字の前までをディレクトリパスとして保存
+		directoryPath = exceptExt.substr(0, pos1 + 1);
+		// 区切り文字の後ろをファイル名として保存
+		fileName = exceptExt.substr(pos1 + 1, filePath_.size() - pos1 - 1);
+		return;
+	}
+	// 区切り文字'/'が出てくる一番最後の部分を検索
+	pos1 = exceptExt.rfind('/');
+	if (pos1 != std::wstring::npos)
+	{
+		// 区切り文字の前までをディレクトリパスとして保存
+		directoryPath = exceptExt.substr(0, pos1 + 1);
+		// 区切り文字の後ろをファイル名として保存
+		fileName = exceptExt.substr(pos1 + 1, filePath_.size() - pos1 - 1);
+		return;
+	}
+	// 区切り文字がないのでファイル名のみとして扱う
+	directoryPath = L"";
+	fileName = exceptExt;
+}
+
+void TextureConverter::SaveDDSTextureToFile()
+{
+	// 読み込んだテクスチャをSRGBとして扱う
+	metadata.format = MakeSRGB(metadata.format);
+	
+	// 出力ファイル名を設定する
+	std::wstring filePath = directoryPath + fileName + L".dds";
+	
+	// DDSファイル書き出し
+	HRESULT result = SaveToDDSFile(scratchImage.GetImages(), 
+		scratchImage.GetImageCount(), metadata, DDS_FLAGS_NONE, filePath.c_str());
+	assert(SUCCEEDED(result));
+}
+
 void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath)
 {
 	// テクスチャファイルを読み込む
 	LoadWICTextureFromFile(filePath);
+	
 	// DDS形式に変換して書き出す
+	SaveDDSTextureToFile();
 }
